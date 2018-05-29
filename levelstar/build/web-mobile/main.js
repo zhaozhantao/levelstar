c23426ea0c317b9011bd803de0c5702e199309a1
赵占涛 <369880281@qq.com>
2018-05-29 08:38:05 +0800

    布上去

--- levelstar/build/web-mobile/main.js
+++ levelstar/build/web-mobile/main.js
@@ -1,23 +1,45 @@
 (function () {
 
-    'use strict';
-
     function boot () {
 
         var settings = window._CCSettings;
         window._CCSettings = undefined;
 
         if ( !settings.debug ) {
-            // retrieve minified raw assets
+            var uuids = settings.uuids;
+
             var rawAssets = settings.rawAssets;
             var assetTypes = settings.assetTypes;
+            var realRawAssets = settings.rawAssets = {};
             for (var mount in rawAssets) {
                 var entries = rawAssets[mount];
-                for (var uuid in entries) {
-                    var entry = entries[uuid];
+                var realEntries = realRawAssets[mount] = {};
+                for (var id in entries) {
+                    var entry = entries[id];
                     var type = entry[1];
+                    // retrieve minified raw asset
                     if (typeof type === 'number') {
                         entry[1] = assetTypes[type];
+                    }
+                    // retrieve uuid
+                    realEntries[uuids[id] || id] = entry;
+                }
+            }
+
+            var scenes = settings.scenes;
+            for (var i = 0; i < scenes.length; ++i) {
+                var scene = scenes[i];
+                if (typeof scene.uuid === 'number') {
+                    scene.uuid = uuids[scene.uuid];
+                }
+            }
+
+            var packedAssets = settings.packedAssets;
+            for (var packId in packedAssets) {
+                var packedIds = packedAssets[packId];
+                for (var j = 0; j < packedIds.length; ++j) {
+                    if (typeof packedIds[j] === 'number') {
+                        packedIds[j] = uuids[packedIds[j]];
                     }
                 }
             }
@@ -28,6 +50,16 @@
 
         if (cc.sys.isBrowser) {
             canvas = document.getElementById('GameCanvas');
+        }
+
+        if (false) {
+            var ORIENTATIONS = {
+                'portrait': 1,
+                'landscape left': 2,
+                'landscape right': 3
+            };
+            BK.Director.screenMode = ORIENTATIONS[settings.orientation];
+            initAdapter();
         }
 
         function setLoadingDisplay () {
@@ -50,29 +82,37 @@
 
         var onStart = function () {
             cc.view.resizeWithBrowserSize(true);
-            // UC browser on many android devices have performance issue with retina display
-            if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
-                cc.view.enableRetina(true);
-            }
-            //cc.view.setDesignResolutionSize(settings.designWidth, settings.designHeight, cc.ResolutionPolicy.SHOW_ALL);
 
-            if (cc.sys.isBrowser) {
-                setLoadingDisplay();
-            }
+            if (!false && !false) {
+                // UC browser on many android devices have performance issue with retina display
+                if (cc.sys.os !== cc.sys.OS_ANDROID || cc.sys.browserType !== cc.sys.BROWSER_TYPE_UC) {
+                    cc.view.enableRetina(true);
+                }
+                if (cc.sys.isBrowser) {
+                    setLoadingDisplay();
+                }
 
-            if (cc.sys.isMobile) {
-                if (settings.orientation === 'landscape') {
-                    cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+                if (cc.sys.isMobile) {
+                    if (settings.orientation === 'landscape') {
+                        cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
+                    }
+                    else if (settings.orientation === 'portrait') {
+                        cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+                    }
+                    cc.view.enableAutoFullScreen([
+                        cc.sys.BROWSER_TYPE_BAIDU,
+                        cc.sys.BROWSER_TYPE_WECHAT,
+                        cc.sys.BROWSER_TYPE_MOBILE_QQ,
+                        cc.sys.BROWSER_TYPE_MIUI,
+                    ].indexOf(cc.sys.browserType) < 0);
                 }
-                else if (settings.orientation === 'portrait') {
-                    cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
+
+                // Limit downloading max concurrent task to 2,
+                // more tasks simultaneously may cause performance draw back on some android system / brwosers.
+                // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
+                if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_ANDROID) {
+                    cc.macro.DOWNLOAD_MAX_CONCURRENT = 2;
                 }
-                // qq, wechat, baidu
-                cc.view.enableAutoFullScreen(
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_BAIDU &&
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_WECHAT &&
-                    cc.sys.browserType !== cc.sys.BROWSER_TYPE_MOBILE_QQ
-                );
             }
 
             // init assets
@@ -80,15 +120,17 @@
                 libraryPath: 'res/import',
                 rawAssetsBase: 'res/raw-',
                 rawAssets: settings.rawAssets,
-                packedAssets: settings.packedAssets
+                packedAssets: settings.packedAssets,
+                md5AssetsMap: settings.md5AssetsMap
             });
+
+            if (false) {
+                cc.Pipeline.Downloader.PackDownloader._doPreload("WECHAT_SUBDOMAIN", settings.WECHAT_SUBDOMAIN_DATA);
+            }
 
             var launchScene = settings.launchScene;
 
             // load scene
-            if (cc.runtime) {
-                cc.director.setRuntimeLaunchScene(launchScene);
-            }
             cc.director.loadScene(launchScene, null,
                 function () {
                     if (cc.sys.isBrowser) {
@@ -100,10 +142,6 @@
                         }
                     }
                     cc.loader.onProgress = null;
-
-                    // play game
-                    // cc.game.resume();
-
                     console.log('Success to load scene: ' + launchScene);
                 }
             );
@@ -111,20 +149,28 @@
 
         // jsList
         var jsList = settings.jsList;
-        var bundledScript = settings.debug ? 'project.dev.js' : 'project.js';
-        if (jsList) {
-            jsList.push(bundledScript);
+
+        if (false) {
+            BK.Script.loadlib();
         }
-        else {
-            jsList = [bundledScript];
+        else
+        {
+            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+            if (jsList) {
+                jsList = jsList.map(function (x) {
+                    return 'src/' + x;
+                });
+                jsList.push(bundledScript);
+            }
+            else {
+                jsList = [bundledScript];
+            }
         }
 
         // anysdk scripts
         if (cc.sys.isNative && cc.sys.isMobile) {
-            jsList = jsList.concat(['jsb_anysdk.js', 'jsb_anysdk_constants.js']);
+            jsList = jsList.concat(['src/anysdk/jsb_anysdk.js', 'src/anysdk/jsb_anysdk_constants.js']);
         }
-
-        jsList = jsList.map(function (x) { return 'src/' + x; });
 
         var option = {
             //width: width,
@@ -132,15 +178,43 @@
             id: 'GameCanvas',
             scenes: settings.scenes,
             debugMode: settings.debug ? cc.DebugMode.INFO : cc.DebugMode.ERROR,
-            showFPS: settings.debug,
+            showFPS: (!false && !false) && settings.debug,
             frameRate: 60,
             jsList: jsList,
             groupList: settings.groupList,
             collisionMatrix: settings.collisionMatrix,
             renderMode: 0
-        };
+        }
 
         cc.game.run(option, onStart);
+    }
+
+    if (false) {
+        BK.Script.loadlib('GameRes://libs/qqplay-adapter.js');
+        BK.Script.loadlib('GameRes://src/settings.js');
+        BK.Script.loadlib();
+        BK.Script.loadlib('GameRes://libs/qqplay-downloader.js');
+        qqPlayDownloader.REMOTE_SERVER_ROOT = "";
+        var prevPipe = cc.loader.md5Pipe || cc.loader.assetLoader;
+        cc.loader.insertPipeAfter(prevPipe, qqPlayDownloader);
+        // <plugin script code>
+        boot();
+        return;
+    }
+
+    if (false) {
+        require(window._CCSettings.debug ? 'cocos2d-js.js' : 'cocos2d-js-min.js');
+        var prevPipe = cc.loader.md5Pipe || cc.loader.assetLoader;
+        cc.loader.insertPipeAfter(prevPipe, wxDownloader);
+        boot();
+        return;
+    }
+
+    if (window.jsb) {
+        require('src/settings.js');
+        require('src/jsb_polyfill.js');
+        boot();
+        return;
     }
 
     if (window.document) {
@@ -154,16 +228,11 @@
         var engineLoaded = function () {
             document.body.removeChild(cocos2d);
             cocos2d.removeEventListener('load', engineLoaded, false);
+            window.eruda && eruda.init() && eruda.get('console').config.set('displayUnenumerable', false);
             boot();
         };
         cocos2d.addEventListener('load', engineLoaded, false);
         document.body.appendChild(cocos2d);
-    }
-    else if (window.jsb) {
-        require('src/settings.js');
-        require('src/jsb_polyfill.js');
-
-        boot();
     }
 
 })();
